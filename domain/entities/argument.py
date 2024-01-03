bfc2b90bf7618755a3da86c43e405303f1844e27
m-al-quadrat <marc.micolau@upc.edu>
2024-01-03 12:22:13 +0100

    Implemented argument

--- domain/entities/argument.py
+++ domain/entities/argument.py
@@ -4,5 +4,15 @@ This file contains the argument class.
 
 import abc
 
-class Argument(abc):
+class Argument(abc.ABC):
+    """
+    This class is an interface for the arguments.
+    The method must be implemented in the subclasses.
+    """
+    
+    def get_values_as_argument(self):
+        """
+        This method returns the values of the argument.
+        """
+        pass
     
