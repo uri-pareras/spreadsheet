f963696a561741d7208cb92eb79db16737ba5457
m-al-quadrat <marc.micolau@upc.edu>
2024-01-05 17:50:27 +0100

    Mogut evaluate_postfix_expression i generate_postfix_expression a shunting_yard_algorithm.py

--- domain/entities/formula_evaluator.py
+++ domain/entities/formula_evaluator.py
@@ -1,13 +1,14 @@
 """
 This file contains the formula evaluator class and its subclasses. 
 """
-from formula_component import FormulaComponent, Parenthesis
+from formula_component import Parenthesis
 from formula_operator import Operator
 from operand import Operand
-from domain.utils.tokenizer import Tokenizer, Token, TokenType
+from domain.utils.tokenizer import Tokenizer
 from domain.utils.parser import Parser
 import abc
 from value import NumericalValue
+from domain.utils.shunting_yard_algorithm import ShuntingYard
 
 
 class FormulaEvaluator(abc.ABC):
@@ -17,6 +18,7 @@ class FormulaEvaluator(abc.ABC):
     def __init__(self) -> None:
         self.tokenizer = Tokenizer()
         self.parser = Parser()
+        self.shunting_yard = ShuntingYard()
         # self.dependency_manager = DependencyManager()
 
     def generate_expression(self, formula: str) -> list:
@@ -29,7 +31,7 @@ class FormulaEvaluator(abc.ABC):
         """
         tokens = self.tokenizer.tokenize(formula)
         tokens = self.parser.parse(tokens)
-        #TODO: Manage dependencies
+        # TODO: Manage dependencies
         return tokens
 
     @abc.abstractmethod
@@ -61,63 +63,15 @@ class FormulaEvaluatorPostfix(FormulaEvaluator):
         return -- the result of the evaluation (float)
         """
         expression = self.convert_to_formula_components(formula)
-        expression = self.generate_postfix_expression(expression)
-        return self.evaluate_postfix_expression(expression)
+        expression = self.shunting_yard.generate_postfix_expression(expression)
+        return self.shunting_yard.evaluate_postfix_expression(expression)
         pass
 
     def convert_to_formula_components(self, tokens: list) -> list:
-        '''
+        """
         This method converts the list of tokens to a list of FormulaComponent objects.
-        '''
+        """
         pass  # Todo: Aquesta funció ha de convertir els tokens a FormulaComponent objects
-
-
-    def generate_postfix_expression(self, expression: list) -> list:
-        """
-        This method generates the postfix expression from the list of FormulaComponent
-        objects passed as argument. It uses the Shunting-yard algorithm, and outputs
-        a list of FormulaComponent objects. This list alternates between Operand
-        and Operator objects.
-
-        Keyword arguments:
-        expression -- the list of FormulaComponent objects (list of FormulaComponent objects
-        return -- the postfix expression (list of FormulaComponent objects)
-        """
-        output = []
-        stack = []
-        for component in expression:
-            if isinstance(component,Operand):
-                output.append(component)
-            elif isinstance(component, Operator):
-                while stack and isinstance(stack[-1],Operator):
-                    if component.precedence() <= stack[-1].precedence():
-                        output.append(stack.pop())
-                    else:
-                        break
-                stack.append(component)
-            elif isinstance(component, Parenthesis) and component.opens():
-                stack.append(component)
-            elif isinstance(component, Parenthesis) and not component.opens():
-                while stack and isinstance(stack[-1],Parenthesis) and stack[-1].opens():
-                    output.append(stack.pop())
-                stack.pop()
-
-        return output + stack[::-1]
-
-
-    def evaluate_postfix_expression(self, expression: list) -> float:
-        """
-        This method evaluates the postfix expression passed as argument.
-        """
-        stack = []
-        for component in expression:
-            if isinstance(component,Operand):
-                stack.append(component)
-            elif isinstance(component,Operator):
-                operand2 = stack.pop().get_value_as_operand()
-                operand1 = stack.pop().get_value_as_operand()
-                stack.append(component.compute(operand1,operand2))
-        return stack.pop().value
 
 
 if __name__ == "__main__":
@@ -125,7 +79,7 @@ if __name__ == "__main__":
     expr = [Parenthesis(opens=True),NumericalValue(5), Operator("*"), NumericalValue(4), Operator("+"),
             NumericalValue(3), Operator("*"), NumericalValue(2), Parenthesis(opens=False), Operator("-"), NumericalValue(1)]
     f = FormulaEvaluatorPostfix()
-    postfix_expr = f.generate_postfix_expression(expr)
+    postfix_expr = f.shunting_yard.generate_postfix_expression(expr)
     for item in postfix_expr:
         try:
             print(item.value)
--- domain/utils/shunting_yard_algorithm.py
+++ domain/utils/shunting_yard_algorithm.py
@@ -1,51 +1,68 @@
 """
 This file contains the shunting yard algorithm class implementation.
 """
-
-from domain.entities.formula_evaluator import FormulaEvaluator
-from parser import TokenType
+from domain.entities.formula_component import Parenthesis
+from domain.entities.operand import Operand
+from domain.entities.formula_operator import Operator
 
 
 class ShuntingYard:
     """
     This class implements the shunting yard algorithm.
+    It implements the following methods:
+    - generate_postfix_expression: generates the postfix expression from the list of FormulaComponent
+    - evaluate_postfix_expression: evaluates the postfix expression
     """
 
-    def __init__(self):
+    @staticmethod
+    def generate_postfix_expression(expression: list) -> list:
         """
-        This method initializes the shunting yard algorithm.
-        """
-        self._output_queue = []
-        self._operator_stack = []
-
-    def clean(self):
-        """
-        This method cleans the shunting yard algorithm.
-        """
-        self._output_queue = []
-        self._operator_stack = []
-
-    def evaluate_shunting_yard(self, token_list: list):
-        """
-        Given a token list this method evaluates the shunting yard algorithm.
-        This will return the result of the computation.
+        This method generates the postfix expression from the list of FormulaComponent
+        objects passed as argument. It uses the Shunting-yard algorithm, and outputs
+        a list of FormulaComponent objects. This list alternates between Operand
+        and Operator objects.
 
         Keyword arguments:
-        token_list -- the token list (list of tokens)
-        return -- the result of the computation (float)
+        expression -- the list of FormulaComponent objects (list of FormulaComponent objects
+        return -- the postfix expression (list of FormulaComponent objects)
         """
-        """self.clean()
-        for token in token_list:
-            if token.type == TokenType.NUMBER:   # We will only have numbers
-                self._output_queue.append(token)
-            elif token.type == TokenType.OPERATOR:
-                self._operator_stack.append(token)
-            elif token.type == TokenType.OPENING_PARENTHESIS:
-                self._operator_stack.append(token)
-            elif token.type == TokenType.CLOSING_PARENTHESIS:
-                while self._operator_stack[-1].type != TokenType.OPENING_PARENTHESIS:
-                    self._output_queue.append(self._operator_stack.pop())
-                self._operator_stack.pop()
-            else:
-                raise SyntaxError("Invalid token")"""
-        pass
+        output = []
+        stack = []
+        for component in expression:
+            if isinstance(component, Operand):
+                output.append(component)
+            elif isinstance(component, Operator):
+                while stack and isinstance(stack[-1], Operator):
+                    if component.precedence() <= stack[-1].precedence():
+                        output.append(stack.pop())
+                    else:
+                        break
+                stack.append(component)
+            elif isinstance(component, Parenthesis) and component.opens():
+                stack.append(component)
+            elif isinstance(component, Parenthesis) and not component.opens():
+                while stack and isinstance(stack[-1], Parenthesis) and stack[-1].opens():
+                    output.append(stack.pop())
+                stack.pop()
+
+        return output + stack[::-1]
+
+    @staticmethod
+    def evaluate_postfix_expression(expression: list) -> float:
+        """
+        This method evaluates the postfix expression passed as argument.
+        It returns the result of the evaluation.
+
+        Keyword arguments:
+        expression -- the postfix expression (list of FormulaComponent objects)
+        return -- the result of the evaluation (float)
+        """
+        stack = []
+        for component in expression:
+            if isinstance(component, Operand):
+                stack.append(component)
+            elif isinstance(component, Operator):
+                operand2 = stack.pop().get_value_as_operand()
+                operand1 = stack.pop().get_value_as_operand()
+                stack.append(component.compute(operand1, operand2))
+        return stack.pop().value
