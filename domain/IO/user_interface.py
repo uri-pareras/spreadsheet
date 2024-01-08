8089a48d6a12b5bc67c72ef528c67ef5fd71feb1
Marc Micolau <marc.micolau@upc.edu>
2024-01-08 20:52:53 +0100

    Esbós user interface

--- domain/IO/user_interface.py
+++ domain/IO/user_interface.py
@@ -9,52 +9,23 @@ class UserInterface(abc.ABC):
     This class represents an abstract user interface.
     """
     @abc.abstractmethod
-    def run(self) -> None:
+    def get_command(self) -> str:
         """
-        This method runs the user interface.
-        """
-        pass
-
-    def read_commands_from_file(self, file_path: str) -> None:
-        """
-        This method reads the commands from a file.
-
-        Keyword arguments:
-        file_path -- the path of the file (str)
+        This method prints the available commands and gets the command from the user.
         """
         pass
 
-    def create_new_spreadsheet(self) -> None:
+    @abc.abstractmethod
+    def render_spreadsheet(self) -> None:
         """
-        This method creates a new spreadsheet.
-        """
-        pass
-
-    def edit_cell(self, cell_identifier: str, new_content: str) -> None:
-        """
-        This method edits a cell.
-
-        Keyword arguments:
-        cell_identifier -- the identifier of the cell (str)
-        new_content -- the new content of the cell (str)
+        This method prints the spreadsheet.
         """
         pass
 
-    def load_spreadsheet_from_file(self, file_path: str) -> None:
+    @abc.abstractmethod
+    def render_available_commands(self) -> None:
         """
-        This method loads a spreadsheet from a file.
-
-        Keyword arguments:
-        file_path -- the path of the file (str)
-        """
-        pass
-
-    def save_spreadsheet_to_file(self, file_path: str) -> None:
-        """
-        This method saves the spreadsheet to a file.
-
-        Keyword arguments:
-        file_path -- the path of the file (str)
+        This method prints the available commands.
         """
         pass
 
@@ -64,8 +35,72 @@ class TextualUserInterface(UserInterface):
     This class represents a concrete implementation of the user interface.
     It represents a textual user interface.
     """
-    def run(self) -> None:
+    def __init__(self):
         """
-        This method runs the user interface.
+        This method initializes the user interface.
+        """
+        self._available_commands = ["RF", "C", "E", "L", "S", "Q"]
+
+    def get_command(self) -> list:
+        """
+        This method prints the available commands and gets the command from the user.
+        """
+        self.render_available_commands()
+        command = input("Enter a command: ")
+        command = command.strip()
+        command = command.upper()
+        command_list = command.split(" ")
+        if self.is_command_valid(command_list):
+            return command_list
+
+    def is_command_valid(self, command: list) -> bool:
+        """
+        This method checks if a command is valid.
+
+        Keyword arguments:
+        command -- the command (str)
+        return -- True if the command is valid, False otherwise (bool)
+        """
+        if command[0].upper() in self._available_commands:
+            if command[0].upper() in ["RF", "L", "S"]:
+                if len(command) == 2:
+                    return True
+                else:
+                    print("Expected 1 argument ", len(command) - 1, " given")
+                    return False
+            elif command[0].upper() in ["C", "Q"]:
+                if len(command) == 1:
+                    return True
+                else:
+                    print("Expected 0 arguments ", len(command) - 1, " given")
+                    return False
+            elif command[0].upper() == "E":
+                if len(command) == 3:
+                    return True
+                else:
+                    print("Expected 2 arguments ", len(command) - 1, " given")
+                    return False
+        else:
+            print("Invalid command.")
+            return False
+
+    def render_spreadsheet(self) -> None:
+        """
+        This method prints the spreadsheet.
         """
         pass
+
+    def render_available_commands(self) -> None:
+        """
+        This method prints the available commands.
+        """
+        print("-------------------------------------------------------------------------------------------------------")
+        print("Available commands: ")
+        print("RF - Read file: This command shall have one argument: the text file pathname.")
+        print("C - Create new spreadsheet")
+        print("E - Edit cell: The first argument shall be a cell coordinate. The second one shall be the content.")
+        print("L - Load spreadsheet from file")
+        print("S - Save spreadsheet to file")
+        print("Q - Quit")
+        print("IMPORTANT: COMMANDS AND ARGUMENTS MUST BE SEPARATED BY A SPACE.")
+        print("-------------------------------------------------------------------------------------------------------")
